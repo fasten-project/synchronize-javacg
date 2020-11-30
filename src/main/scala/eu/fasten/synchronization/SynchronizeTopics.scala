37ad932a1acc4c0c9446ac756bf8e02c2ae78722
Wouter Zorgdrager <zorgdragerw@gmail.com>
2020-11-30 16:07:56 +0100

    Added handling of the timer.

--- src/main/scala/eu/fasten/synchronization/SynchronizeTopics.scala
+++ src/main/scala/eu/fasten/synchronization/SynchronizeTopics.scala
@@ -55,13 +55,20 @@ class SynchronizeTopics(environment: Environment)
       ctx: KeyedProcessFunction[String, ObjectNode, ObjectNode]#Context,
       out: Collector[ObjectNode]): Unit = {
 
+    print("TIMESTAMP CHECK ")
+    println(value.get("metadata").get("timestamp").asLong() == ctx.timestamp())
+
+    // Current time and the time of the event.
+    val currentTime = System.currentTimeMillis()
+    val timestamp = ctx.timestamp()
+
     val topic = value
       .get("metadata")
       .get("topic")
       .asText()
 
     logger.info(
-      f"[INCOMING] [${ctx.getCurrentKey}] [$topic] [${value.get("metadata").get("timestamp").asText()}] [-1i] [NONE]")
+      f"[INCOMING] [${ctx.getCurrentKey}] [$topic] [${timestamp}i] [-1i] [NONE]")
 
     if (topic == environment.topicOne) {
 
@@ -74,7 +81,6 @@ class SynchronizeTopics(environment: Environment)
 
       // We already have the data from the other topic! Join time :)
       if (topicTwoCurrentState != null) {
-        val currentTime = System.currentTimeMillis()
         val stateTimestamp = topicTwoCurrentState
           .get("state_timestamp")
           .asLong()
@@ -83,7 +89,11 @@ class SynchronizeTopics(environment: Environment)
         val duration = currentTime - stateTimestamp
 
         logger.info(
-          f"[JOIN] [${ctx.getCurrentKey}] [BOTH] [${value.get("metadata").get("timestamp").asText()}] [${duration}i] [NONE]")
+          f"[JOIN] [${ctx.getCurrentKey}] [BOTH] [${value.get("metadata").get("timestamp").asText()}i] [${duration}i] [NONE]")
+
+        // Get timestamp of the record from topic two.
+        val topicTwoRecordTimestamp =
+          topicTwoCurrentState.get("metadata").get("timestamp").asLong()
 
         // Remove metadata.
         topicTwoCurrentState.remove("state_timestamp")
@@ -103,16 +113,30 @@ class SynchronizeTopics(environment: Environment)
         ctx
           .timerService()
           .deleteEventTimeTimer(
-            stateTimestamp + (environment.windowTime * 1000))
+            topicTwoRecordTimestamp + (environment.windowTime * 1000))
 
         // Empty the state.
-        topicTwoState.update(null)
+        topicTwoState.clear()
 
         return
       } else { // The state in topic two is still empty, let's add to state one.
-        if (topicOneState.value() != null) { // There is a duplicate, because the state is not null. We just override.
-
+        if (topicOneState.value() != null) { // There is a duplicate, because the state is not null. We just override it.
+          val duplicateDuration = currentTime - topicOneState
+            .value()
+            .get("state_timestamp")
+            .asLong()
+          logger.warn(
+            f"[DUPLICATE] [${ctx.getCurrentKey}] [$topic] [${value.get("metadata").get("timestamp").asText()}i] [${duplicateDuration}i] [NONE]")
         }
+
+        // Update state, add field with current timestamp (that's nice to know for the monitoring).
+        value.put("state_timestamp", currentTime.toString)
+        topicOneState.update(value)
+
+        // Set a timer, to ensure that this message is joined within {windowTime} seconds.
+        ctx
+          .timerService()
+          .registerEventTimeTimer(timestamp + (environment.windowTime * 1000))
       }
 
     } else if (topic == environment.topicTwo) {} else {
@@ -120,11 +144,58 @@ class SynchronizeTopics(environment: Environment)
       val exception = new JobException(
         s"Expected a message from ${environment.topicOne} or ${environment.topicTwo}, but received from $topic.")
       logger.info(
-        f"[INCOMING] [${ctx.getCurrentKey}] [$topic] [${value.get("metadata").get("timestamp").asText()} [-1i] [JobException]",
+        f"[INCOMING] [${ctx.getCurrentKey}] [$topic] [${value.get("metadata").get("timestamp").asText()}i] [-1i] [JobException]",
         exception)
 
       throw exception
     }
+  }
 
+  override def onTimer(
+      timestamp: Long,
+      ctx: KeyedProcessFunction[String, ObjectNode, ObjectNode]#OnTimerContext,
+      out: Collector[ObjectNode]): Unit = {
+    // A timer is created when state is added. If this timer is called, we need to expire the state and output an error.
+
+    val topicOneCurrentState = topicOneState.value()
+    val topicTwoCurrentState = topicTwoState.value()
+
+    if (topicOneCurrentState != null && topicTwoCurrentState != null) { // Both states are filled, this should not be possible.
+      val duration = Math.max(
+        topicOneCurrentState.get("state_timestamp").asLong(),
+        topicTwoCurrentState.get("state_timestamp").asLong()) - Math.min(
+        topicOneCurrentState.get("state_timestamp").asLong(),
+        topicTwoCurrentState.get("state_timestamp").asLong())
+      logger.warn(
+        f"[EXPIRE] [${ctx.getCurrentKey}] [BOTH] [${topicOneCurrentState.get("metadata").get("timestamp").asText()}i] [${duration}i] [NONE]")
+
+      // Remove metadata.
+      topicOneCurrentState.remove("state_timestamp")
+      topicOneCurrentState.remove("metadata")
+      topicTwoCurrentState.remove("state_timestamp")
+      topicTwoCurrentState.remove("metadata")
+
+      // Build an output record.
+      val outputRecord = mapper.createObjectNode()
+      outputRecord.put("key", ctx.getCurrentKey)
+      outputRecord.set(environment.topicOne, topicOneCurrentState)
+      outputRecord.set(environment.topicTwo, topicTwoCurrentState)
+
+      // Collect the record.
+      out.collect(outputRecord)
+
+    } else if (topicOneCurrentState != null) {
+      logger.warn(
+        f"[EXPIRE] [${ctx.getCurrentKey}] [${environment.topicOne}] [${topicOneCurrentState.get("metadata").get("timestamp").asText()}i] [-1i] [NONE]")
+
+    } else if (topicTwoCurrentState != null) {
+      logger.warn(
+        f"[EXPIRE] [${ctx.getCurrentKey}] [${environment.topicTwo}] [${topicTwoCurrentState.get("metadata").get("timestamp").asText()}i] [-1i] [NONE]")
+    } else {
+      logger.warn(f"[EXPIRE] [${ctx.getCurrentKey}] [NONE] [0i] [-1i] [NONE]")
+    }
+
+    topicOneState.clear()
+    topicTwoState.clear()
   }
 }
